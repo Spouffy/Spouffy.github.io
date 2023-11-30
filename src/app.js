a3d4b500d482cc5df3d18e2c1f1cfea05fa3247b
Spouffy <65297560+Spouffy@users.noreply.github.com>
2023-11-30 01:13:04 +0100

    Science project

--- src/app.js
+++ src/app.js
@@ -1,13 +1,38 @@
-console.log('Welcome to the console :)')
+let bloodSugar = 1
+let liverContent = 0
+let normalSugarRange = [0.65, 1.1]
 
-const body = slct('#bod')
-var hert
+var bloodSugarDis = µ('#bloodsugar')
 
-on('click', body, (e) => {
-  hert = crtElm('img', 'bod')
-  console.log(hert);
-  hert.src = 'src/assets/heart.png'
-  hert.className = 'hearts'
-  hert.style.top = (e.y-25)+'px'
-  hert.style.left = (e.x-25)+'px'
-})
+function addSugar (e) {
+    bloodSugar = bloodSugar + e
+    if (bloodSugar > normalSugarRange[1]) {
+        console.log('Start USING sugar');
+    } else if (true) {
+
+    }
+    log(`Adding sugar : ${e}g/l \n Current sugar : ${bloodSugar}g/l`)
+
+}
+
+function useSugar(e) {
+    bloodSugar = bloodSugar - e
+    if (bloodSugar < normalSugarRange[0]) {
+        console.log('Start STOCKING sugar');
+    } else if (true) {
+        
+    }
+    log(`Using sugar : ${e}g/l \n Current sugar : ${bloodSugar}g/l`)
+}
+
+
+setInterval(spawnGlu, 5000)
+
+function spawnGlu () {
+    var g = new Elm('div', µ('#glu-container').elm)
+    g.className = 'glu'
+    var s = µ(g).crtElm('span');
+    g.style.width = Math.floor(Math.random()*20) +'px'
+    g.style.height = Math.floor(Math.random()*35) +'px'
+    console.log(s);
+}
