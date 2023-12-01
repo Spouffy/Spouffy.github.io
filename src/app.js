de6b163909c1015f7a3a2ba24a333a81dd5f5154
Spouffy <65297560+Spouffy@users.noreply.github.com>
2023-12-01 19:56:31 +0100

    60percent

--- src/app.js
+++ src/app.js
@@ -1,38 +1,93 @@
-let bloodSugar = 1
+let bloodSugar = 10
 let liverContent = 0
-let normalSugarRange = [0.65, 1.1]
+let range = {max: 11, min: 7}
 
-var bloodSugarDis = µ('#bloodsugar')
+let isWorking = false;
+
+const animationTime = 5
+const bloodSugarDis = µ('#bloodsugar')
 
 function addSugar (e) {
     bloodSugar = bloodSugar + e
-    if (bloodSugar > normalSugarRange[1]) {
-        console.log('Start USING sugar');
-    } else if (true) {
-
-    }
-    log(`Adding sugar : ${e}g/l \n Current sugar : ${bloodSugar}g/l`)
-
+    updateSugarDisplay()
 }
 
 function useSugar(e) {
     bloodSugar = bloodSugar - e
-    if (bloodSugar < normalSugarRange[0]) {
+    updateSugarDisplay()
+    if (bloodSugar < range.min) {
         console.log('Start STOCKING sugar');
     } else if (true) {
         
     }
-    log(`Using sugar : ${e}g/l \n Current sugar : ${bloodSugar}g/l`)
 }
 
-
-setInterval(spawnGlu, 5000)
-
+// func that spawn a glucagen element and removes it after the animation time has passed 
 function spawnGlu () {
     var g = new Elm('div', µ('#glu-container').elm)
     g.className = 'glu'
     var s = µ(g).crtElm('span');
     g.style.width = Math.floor(Math.random()*20) +'px'
     g.style.height = Math.floor(Math.random()*35) +'px'
-    console.log(s);
+
+
+    setTimeout(() => {
+        ++bloodSugar
+        updateSugarDisplay()
+        g.remove()
+    }, animationTime*1000);
 }
+
+function spawnIns() {
+    var g = new Elm('div', µ('#ins-container').elm)
+    g.className = 'ins'
+    var s = µ(g).crtElm('span');
+    g.style.width = Math.floor(Math.random()*20) +'px'
+    g.style.height = Math.floor(Math.random()*35) +'px'
+
+    setTimeout(() => {
+        --bloodSugar
+        updateSugarDisplay()
+        g.remove()
+    }, animationTime*1000);
+}
+
+// func that update the sugar blood display 
+function updateSugarDisplay () {
+    bloodSugarDis.elm.innerText = bloodSugar/10
+}
+
+updateSugarDisplay()
+
+
+// Interval that checks if the sugar is low or high and act accordingly
+var checker = setInterval(e => {
+    if (isWorking) {
+        return
+    }
+    if (bloodSugar > range.max && !isWorking) {
+        console.log('Sugar is high : ', bloodSugar);
+        isWorking = true
+
+        spawnIns()
+        isWorking = false;
+
+    }
+    if (bloodSugar < range.min && !isWorking) {
+        console.log('Sugar is low : ', bloodSugar);
+        isWorking = true
+        
+        spawnGlu()
+        isWorking = false;
+
+    }
+    if (bloodSugar >= range.min && bloodSugar <= range.max) {
+        console.log('Range is normal : ', bloodSugar);
+        isWorking = false;
+
+    } 
+
+    bloodSugar > range.max ? bloodSugarDis.elm.style.backgroundColor = "red" : null
+    bloodSugar < range.min ? bloodSugarDis.elm.style.backgroundColor = "red" : null
+    bloodSugar >= range.min && bloodSugar <= range.max ? bloodSugarDis.elm.style.backgroundColor = "white" : null
+}, 200)
